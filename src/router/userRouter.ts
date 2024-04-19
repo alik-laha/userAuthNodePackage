3f0879ed8c5b1c19c892ae18325e0f88b7c47bf3
alik-laha <aliklaha@outlook.com>
2024-04-19 08:02:44 +0530

    Verify is ready

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -3,9 +3,13 @@ const router = express.Router();
 import Signup from "../controller/user/Signup.js";
 import CheckUser from "../middleware/checkUser.js";
 import Login from "../controller/user/login.js";
+import Verify from "../controller/user/verify.js";
 
 router.post("/signup", CheckUser, Signup);
 
 router.put("/login", Login)
+
+router.get("/verify", Verify)
+
 
 export default router;
